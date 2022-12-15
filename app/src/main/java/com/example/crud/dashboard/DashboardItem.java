4bb057436effd4a929d9d241f02e430027c24ac9
Anupama <anupama.lakshmi@Improve10x.com>
2022-12-15 12:14:14 +0530

    Done with : DashBoard : change class name Dashboard to DashboardItem

--- app/src/main/java/com/example/crud/dashboard/DashboardItem.java
+++ app/src/main/java/com/example/crud/dashboard/DashboardItem.java
@@ -1,8 +1,7 @@
 package com.example.crud.dashboard;
 
-public class Dashboard {
+public class DashboardItem {
 
-//Todo: change class name Dashboard to DashboardItem
     public String imageUrl;
     public String title;
 }
