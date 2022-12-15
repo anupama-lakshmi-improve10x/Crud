50c86dacb7c3b69030c8f3fdfc5adec017aa0820
Anupama <anupama.lakshmi@Improve10x.com>
2022-12-15 11:48:47 +0530

    Done with : Base Activity : change "MessageActivity"

--- app/src/main/java/com/example/crud/base/BaseActivity.java
+++ app/src/main/java/com/example/crud/base/BaseActivity.java
@@ -30,8 +30,7 @@ public class BaseActivity extends AppCompatActivity {
     }
 
     protected void log(String message) {
-        // Todo: change "MessageActivity"
-        Log.i("MessagesActivity", message);
+        Log.i(this.getClass().getSimpleName(), message);
     }
 
     private void setupCrudApiService() {
